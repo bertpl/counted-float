0e80b781e1a790c5eae8d2a356a1d2c3b11243e9
Bert Pluymers <bert.pluymers@gmail.com>
2025-08-05 13:31:56 +0200

    Create initial public API & add tests.

---
+++ counted_float/__init__.py
@@ -0,0 +1,36 @@
+from ._core.counting import CountedFloat, FlopCountingContext, PauseFlopCounting
+from ._core.counting.models import (
+    FlopCounts,
+    FlopsBenchmarkDurations,
+    FlopsBenchmarkResults,
+    FlopType,
+    FlopWeights,
+    FPUInstruction,
+    SystemInfo,
+)
+
+__all__ = [
+    "CountedFloat",
+    "FlopCountingContext",
+    "FlopCounts",
+    "FlopsBenchmarkDurations",
+    "FlopsBenchmarkResults",
+    "FlopType",
+    "FlopWeights",
+    "FPUInstruction",
+    "PauseFlopCounting",
+    "SystemInfo",
+]
+
+# -------------------------------------------------------------------------
+#  Optional [benchmarking] dependencies
+# -------------------------------------------------------------------------
+from ._core._optional_deps import FLAG_BENCHMARK_DEPS
+
+if FLAG_BENCHMARK_DEPS:
+    import counted_float.benchmarking as benchmarking
+
+    __all__.append("benchmarking")
+
+# delete variable again, we don't want to expose this
+del FLAG_BENCHMARK_DEPS
--- tests/shared/test_optional_deps.py
+++ tests/shared/test_optional_deps.py
@@ -1,3 +1,5 @@
+import importlib
+
 import pytest
 
 from counted_float._core._optional_deps import FLAG_BENCHMARK_DEPS, requires_benchmark_deps
@@ -37,3 +39,22 @@ def test_benchmark_decorator_without_deps():
     """Check if decorator works correctly if [benchmark] dependencies are NOT available."""
     with pytest.raises(ImportError):
         decorated_function(1)  # error only raised when called, not when defined
+
+
+# =================================================================================================
+#  Misc
+# =================================================================================================
+def test_import_all_counted_float():
+    """Check if importing all elements of __all__ from counted_float always works."""
+    from counted_float import __all__ as ALL
+
+    for item in ALL:
+        _ = getattr(importlib.import_module("counted_float"), item)
+
+
+def test_import_all_counted_float_benchmarking():
+    """Check if importing all elements of __all__ from counted_float.benchmarking always works."""
+    from counted_float.benchmarking import __all__ as ALL
+
+    for item in ALL:
+        _ = getattr(importlib.import_module("counted_float.benchmarking"), item)
