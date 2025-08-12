0fb7f13612799b71d69377badbbee0e27799a9a1
Bert Pluymers <bert.pluymers@gmail.com>
2025-08-12 08:53:49 +0200

    Remove unintentionally uploaded files.

--- counted_float/_core/counting/config/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._config import get_flop_weights, set_flop_weights
-from ._defaults import (
-    get_default_consensus_flop_weights,
-    get_default_empirical_flop_weights,
-    get_default_theoretical_flop_weights,
-)
--- counted_float/_core/counting/models/_base.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class MyBaseModel(BaseModel):
-    def show(self):
-        print(self.model_dump_json(indent=4))
