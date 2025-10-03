0c34e724e5808bf2d1bac380a787154cb5d3aeec
Bert Pluymers <bert.pluymers@gmail.com>
2025-10-03 08:38:40 +0200

    Use V2 flops benchmark suite instead of V1 in this user-facing benchmarking command.

--- counted_float/_core/benchmarking/__init__.py
+++ counted_float/_core/benchmarking/__init__.py
@@ -1,16 +1,17 @@
 from importlib.metadata import version
 
 from .flops_v1 import FlopsBenchmarkResults_V1, FlopsBenchmarkSuite_V1
+from .flops_v2 import FlopsBenchmarkResults_V2, FlopsBenchmarkSuite_V2
 
 
-def run_flops_benchmark() -> FlopsBenchmarkResults_V1:
+def run_flops_benchmark() -> FlopsBenchmarkResults_V2:
     """Run the flops benchmark suite with default settings returns a FlopsBenchmarkResults object."""
 
     print()
     print(f"Running FLOPS benchmarks using counted-float {version('counted-float')} ...")
     print()
 
-    benchmark_results = FlopsBenchmarkSuite_V1().run()
+    benchmark_results = FlopsBenchmarkSuite_V2().run()
 
     print()
 
