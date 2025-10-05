f42ab1a5ce9e5693428dae1d48e43afcd4e582a8
Bert Pluymers <bert.pluymers@gmail.com>
2025-10-05 11:20:43 +0200

    Remove usage of StrEnum backport.
    - StrEnum is natively support in all supported Python versions now

--- counted_float/_core/compatibility/__init__.py
+++ counted_float/_core/compatibility/__init__.py
@@ -1,2 +1,1 @@
 from ._numba import is_numba_installed, numba
-from ._strenum import StrEnum
--- counted_float/_core/compatibility/_strenum.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-if sys.version_info >= (3, 11):
-    from enum import StrEnum
-else:
-    from backports.strenum import StrEnum
