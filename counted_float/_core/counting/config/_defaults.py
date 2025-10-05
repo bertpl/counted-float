ae0eaa63f13392039806f3f059aa702f63317a8e
Bert Pluymers <44161457+bertpl@users.noreply.github.com>
2025-10-05 10:34:23 +0200

    Simplify builtin weights (#100)
    Reduce ways in which built-in flop weights can be retrieved.

--- counted_float/_core/counting/config/_defaults.py
+++ counted_float/_core/counting/config/_defaults.py
@@ -10,38 +10,7 @@ def get_default_consensus_flop_weights(rounded: bool = True) -> FlopWeights:
     Get the default CONSENSUS flop weights.
     Computed as the geo-mean of the unrounded empirical and theoretical weights, rounded to the nearest integer.
     """
-    weights = FlopWeights.as_geo_mean(
-        [
-            get_default_empirical_flop_weights(rounded=False),
-            get_default_theoretical_flop_weights(rounded=False),
-        ]
-    )
-    if rounded:
-        return weights.round()
-    else:
-        return weights
-
-
-@cache
-def get_default_empirical_flop_weights(rounded: bool = True) -> FlopWeights:
-    """
-    Get the default EMPIRICAL flop weights.
-    Computed as the geo-mean of flop weights estimated from built-in benchmark results.
-    """
-    weights = BuiltInData.get_flop_weights(key_filter="benchmarks.")
-    if rounded:
-        return weights.round()
-    else:
-        return weights
-
-
-@cache
-def get_default_theoretical_flop_weights(rounded: bool = True) -> FlopWeights:
-    """
-    Get the default THEORETICAL flop weights.
-    Computed as the geo-mean of flop weights estimated from built-in instruction latency analyse.
-    """
-    weights = BuiltInData.get_flop_weights(key_filter="specs.")
+    weights = BuiltInData.get_flop_weights(key_filter="")
     if rounded:
         return weights.round()
     else:
