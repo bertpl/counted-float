5ae50e60d892fea563b937c475c2cdb0de475291
Bert Pluymers <bert.pluymers@gmail.com>
2025-10-09 13:45:25 +0200

    Add utility function for rounding in 3 different ways.

---
+++ tests/utils/test_rounding.py
@@ -0,0 +1,34 @@
+from typing import Literal
+
+import pytest
+
+from counted_float._core.utils import round_number
+
+
+@pytest.mark.parametrize(
+    "value,mode,expected_value",
+    [
+        (1.123, None, 1.123),
+        (0.9, "nearest_int", 1.0),
+        (1.0, "nearest_int", 1.0),
+        (1.7, "nearest_int", 2.0),
+        (1.0, "10%", 1.0),
+        (1.04, "10%", 1.0),
+        (1.05, "10%", 1.1),
+        (1.06, "10%", 1.1),
+        (1.14, "10%", 1.1),
+        (1.15, "10%", 1.2),
+        (1.16, "10%", 1.2),
+        (1.24, "10%", 1.2),
+        (1.25, "10%", 1.3),
+        (8.4, "10%", 8.0),
+        (84, "10%", 80.0),
+        (0.84, "10%", 0.8),
+    ],
+)
+def test_round_number(value: float, mode: None | Literal["nearest_int", "10%"], expected_value: int | float):
+    # --- act ---------------------------------------------
+    rounded = round_number(value, mode)
+
+    # --- assert ------------------------------------------
+    assert rounded == expected_value
