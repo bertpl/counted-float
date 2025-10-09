ae88444572a91ad83cae29cbd71411db9b7ffdd1
Bert Pluymers <44161457+bertpl@users.noreply.github.com>
2025-10-09 18:03:54 +0200

    Add "10%" flop weight rounding (#118)
    Addresses #50

--- counted_float/_core/utils/__init__.py
+++ counted_float/_core/utils/__init__.py
@@ -3,4 +3,5 @@ from ._formatting import format_latency, format_time_duration
 from ._geo_mean import geo_mean
 from ._latency import convert_nsecs_to_cycles
 from ._missing_data import impute_missing_data
+from ._rounding import round_number
 from ._timer import Timer
